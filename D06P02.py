c8a2192950c9e98c95459f6df42c3539ec7ab0dc
Parolo <parolo@pop-os.localdomain>
2021-12-24 11:49:54 +0100

    finish day 6

---
+++ D06P02.py
@@ -0,0 +1,30 @@
+inputFile = open('D06P01.txt', 'r')
+FileLines = inputFile.readlines()
+
+fishAges = FileLines[0].split(",")
+
+fishByAge = [0] * 7
+newbornFish = [0] * 7
+
+for fishAge in fishAges:
+	fishByAge[int(fishAge)] += 1
+
+i = 0
+
+while i < 256:
+	newbornFish[(i + 2) % 7] = fishByAge[i % 7]
+	fishByAge[i % 7] += newbornFish[i % 7]
+	newbornFish[i % 7] = 0
+	
+	i += 1
+
+fishCount = 0
+i = 0
+
+while i < 7:
+	fishCount += fishByAge[i]
+	fishCount += newbornFish[i]
+	
+	i += 1
+
+print("Total fish: ", fishCount)
